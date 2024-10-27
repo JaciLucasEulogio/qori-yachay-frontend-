7bcf06e2fc3a46ada3270ddd38504ac5fb38830f
Jaci Lucas <72617519@continental.edu.pe>
2024-10-27 15:27:11 -0500

    Update favicon link in index.html and change page title to "QoriYachay"

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -36,12 +36,12 @@ export default function LandingPage() {
               </p>
               <div className="mt-10 flex items-center justify-center gap-x-6">
                 <a
-                  href="/form"
-                  className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Pruébalo
-                </a>
-                <a href="https://github.com/JaciLucasEulogio/qori-yachay-frontend-" className="text-sm font-semibold leading-6 text-gray-900">
+                    href="/form"
+                    className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  >
+                    Pruébalo
+                  </a>
+                <a href="https://github.com/JaciLucasEulogio/qori-yachay-frontend-" target="_blank" className="text-sm font-semibold leading-6 text-gray-900">
                   Repositorio <span aria-hidden="true">→</span>
                 </a>
               </div>

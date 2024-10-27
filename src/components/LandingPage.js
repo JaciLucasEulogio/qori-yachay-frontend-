97e5ab87e169b3cd4594f738dcb06e70534c64cc
Jaci Lucas <72617519@continental.edu.pe>
2024-10-27 15:17:29 -0500

    Update link in LandingPage component to redirect to form page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -21,7 +21,7 @@ export default function LandingPage() {
             <div className="hidden sm:mb-8 sm:flex sm:justify-center">
               <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
                 Conoce más sobre nuestra solución.{' '}
-                <a href="/" className="font-semibold text-indigo-600">
+                <a href="/form" className="font-semibold text-indigo-600">
                   <span aria-hidden="true" className="absolute inset-0" />
                   Pruébalo <span aria-hidden="true">&rarr;</span>
                 </a>
